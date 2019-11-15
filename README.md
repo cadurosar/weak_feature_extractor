fff60b2afff63b7d7ef109cee63b85b3b1ae2e1b
Carlos Lassance <cadurosar@gmail.com>
2019-11-15 09:37:04 +0100

    Change to python3

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
-## Details "Knowledge Transfer From Weakly Labeled Audio Using Convolutional Neural Network For Sound Events And Scenes" Anurag Kumar, Maksim Khadkevich, Christian Fügen 
-## ICASSP 2018
+# Details
 
-### https://arxiv.org/pdf/1711.01369.pdf
+Fork from the official github for the "Knowledge Transfer From Weakly Labeled Audio Using Convolutional Neural Network For Sound Events And Scenes" paper. https://github.com/anuragkr90/weak_feature_extractor
 
-## Check out this webpage "http://www.cs.cmu.edu/~alnu/TLWeak.htm" for more results and details
-
-### This code provides the  bare minimum to obtain audio representations using Deep CNN models trained on weakly labeled data (Audioset - Balanced set)
-
-#### 1. call the main function in feat_extractor - returns 1024 or 527 dimensional features
-
-#### 2. It will work with audio of any duration but I would suggest to pad it to make it at least 1.5 seconds for now. 
-
-#### 3. You can turn on gpu use by 'usegpu' variable. Although, for very long audio (more than a few minutes) you might end up getting gpu memory error. 
-
-#### The trained CNN is used to learn meaningful representations for a given audio recording. The classification task can be done by training another classifier on these representations (linear SVMs in this case). 
-
-#### 4. class names and id in classes_id_name.txt for the 527 sounds in audioset over which the model was trained. 
-
-#### 6. Doubts??? -  send me an email. 
+The only differences are that this is adapted to python3 and for our use case. For citations always cite the original work https://arxiv.org/pdf/1711.01369.pdf
--- feature_extractor.py
+++ feature_extractor.py
@@ -9,7 +9,7 @@ from collections import OrderedDict
 import extractor as exm
 
 
-usegpu = False
+usegpu = True
 
 
 n_fft = 1024
@@ -35,67 +35,68 @@ def load_model(netx,modpath):
         new_state_dict[name] = v
     netx.load_state_dict(new_state_dict)
 
-def getFeat(extractor,inpt):
+def get_features(extractor,inpt):
     # return pytorch tensor 
     extractor.eval()
-    indata = Variable(torch.Tensor(inpt),volatile=True)
+    indata = torch.Tensor(inpt)
     if usegpu:
         indata = indata.cuda()
-
-    pred = extractor(indata)
-    print pred.size()
-    if len(pred.size()) > 2:
-        gpred = globalpoolfn(pred,kernel_size=pred.size()[2:])
-        gpred = gpred.view(gpred.size(0),-1)
+    with torch.no_grad():
+        pred = extractor(indata)
+        if len(pred.size()) > 2:
+            gpred = globalpoolfn(pred,kernel_size=pred.size()[2:])
+            gpred = gpred.view(gpred.size(0),-1)
 
     return gpred
 
-#keep sample rate 44100, no need 
-def main(filename,srate=44100):
+def file_to_input(filename,srate=44100):
+
     try:
         y, sr = lib.load(filename,sr=None)
     except:
         raise IOError('Give me an audio  file which I can read!!')
     
     if len(y.shape) > 1:
-        print 'Mono Conversion', 
+        print ('Mono Conversion') 
         y = lib.to_mono(y)
 
     if sr != srate:
-        print 'Resampling to {}'.format(srate),
+        print ('Resampling to {}'.format(srate))
         y = lib.resample(y,sr,srate)
 
         
     mel_feat = lib.feature.melspectrogram(y=y,sr=srate,n_fft=n_fft,hop_length=hop_length,n_mels=128)
-    inpt = lib.power_to_db(mel_feat).T
-    
-    #quick hack for now
-    if inpt.shape[0] < 128:
-        inpt = np.concatenate((inpt,np.zeros((128-inpt.shape[0],n_mels))),axis=0)
-    
+    inpt = lib.power_to_db(mel_feat).T  
 
     # input needs to be 4D, batch_size X 1 X inpt_size[0] X inpt_size[1]
     inpt = np.reshape(inpt,(1,1,inpt.shape[0],inpt.shape[1]))
-    print inpt.shape
+    return inpt
 
-    netType = getattr(netark,trainType)
-    netx = netType(527,netwrkgpl)
-    load_model(netx,pre_model_path)
+def get_extractor(network_architecture=netark, trainType=trainType, global_average_pooling=netwrkgpl, pre_model_path=pre_model_path, use_gpu=usegpu, feature_type=featType):
+    netType = getattr(network_architecture,trainType)
+    net = netType(527,global_average_pooling)
+    load_model(net,pre_model_path)
+    if use_gpu:
+        net.cuda()
+    feat_extractor = exm.featExtractor(net,feature_type)
+        
+    return feat_extractor
 
-    
-    if usegpu:
-        netx.cuda()
-    
-    feat_extractor = exm.featExtractor(netx,featType)
-    
-    pred = getFeat(feat_extractor,inpt)
+def extract_features(_input,**kwargs):
+    extractor = get_extractor(**kwargs)   
+    features = get_features(extractor,_input)
+    return features
 
-    #numpy arrays
-    feature = pred.data.cpu().numpy()
-    print feature.shape
+#keep sample rate 44100, no need 
+def main(filename,srate=44100):
 
-    # prediction for each segment in each column
-    return feature
+    _input = file_to_input(filename,srate)
+    feature = extract_features(_input)
+    extractor = get_extractor()   
+    features = get_features(extractor,_input)
+    features = features.cpu().numpy()
+
+    return features
 
 
 if __name__ == '__main__':
